e89c6843fe73dc70c56336b9353732176bfa70a2
UbendranRG <100841792+UbendranRG@users.noreply.github.com>
2022-05-25 07:21:38 -0700

    Update Print.java

--- com.practice/src/test/java/javaProgram/Print.java
+++ com.practice/src/test/java/javaProgram/Print.java
@@ -5,6 +5,8 @@ public class Print {
 	public static void main(String[] args) {
 System.out.println("hai welcome to git");
 		System.out.println("hai welcome to git again from GIT");
+		System.out.println("hai welcome to git from jenkins");
+		
 	}
 
 }
