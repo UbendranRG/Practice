7d0bb5a26689815db30fb0e4505cc467c16ae82d
Vijayan <Vijayan@DESKTOP-QRLGEK7>
2022-05-25 06:57:45 -0700

    commitNo1

---
+++ com.practice/src/test/java/javaProgram/Swap.java
@@ -0,0 +1,16 @@
+package javaProgram;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		
+		int a=0;
+		int b=1;
+		
+		
+		
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
